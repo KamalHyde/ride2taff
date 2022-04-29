5e4e409c4fb9596f91d9ddd98db76e8d4d31ea92
KamalHyde <benhamouda.kamal@hotmail.fr>
2022-04-29 11:09:26 +0200

    first commit

---
+++ src/main/java/com/example/ride2taff/Ride2taffApplication.java
@@ -0,0 +1,13 @@
+package com.example.ride2taff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ride2taffApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ride2taffApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ride2taff/Ride2taffApplicationTests.java
@@ -0,0 +1,10 @@
+package com.example.ride2taff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ride2taffApplicationTests {
+
+
+}
